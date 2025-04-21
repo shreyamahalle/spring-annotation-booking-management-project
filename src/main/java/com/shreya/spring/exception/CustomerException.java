ef410e785d73b8d7877b8cc4fd2149cd536d02ec
shreya mahalle <shreyamahalle2000@gmail.com>
2025-04-21 11:44:40 +0530

    changes added

--- src/main/java/com/shreya/spring/exception/CustomerException.java
+++ src/main/java/com/shreya/spring/exception/CustomerException.java
@@ -1,8 +1,8 @@
 package com.shreya.spring.exception;
 
-
 public class CustomerException extends RuntimeException {
-    public CustomerException(String NotAvailable) {
+    public CustomerException(String NotAvailable)
+    {
         super(NotAvailable);
     }
 }
